40aacf37d8f3847c770af11982c7b5daa37a2b1b
Nikola <47610156+PULK4N@users.noreply.github.com>
2026-08-22 12:09:03 +0200

    added error message to outbox payload

--- API/PostgreSqlModule/Migrations/EventSourcing/20260822100801_AddedErrorMessageToOutboxPayload.Designer.cs
+++ API/PostgreSqlModule/Migrations/EventSourcing/20260822100801_AddedErrorMessageToOutboxPayload.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NpgsqlTypes;
@@ -13,9 +14,11 @@ using PostgreSqlModule;
 namespace PostgreSqlModule.Migrations.EventSourcing
 {
     [DbContext(typeof(PostgreSqlEventSourcingDbContext))]
-    partial class PostgreSqlEventSourcingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260822100801_AddedErrorMessageToOutboxPayload")]
+    partial class AddedErrorMessageToOutboxPayload
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ API/PostgreSqlModule/Migrations/EventSourcing/20260822100801_AddedErrorMessageToOutboxPayload.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PostgreSqlModule.Migrations.EventSourcing
+{
+    /// <inheritdoc />
+    public partial class AddedErrorMessageToOutboxPayload : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Error",
+                table: "SerializedPayloadMessage",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Error",
+                table: "SerializedPayloadMessage");
+        }
+    }
+}
